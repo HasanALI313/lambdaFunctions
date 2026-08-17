8d403a07eec04450291c6292670743e327b4df6f
HasanALI313 <hasanyounishasan@gmail.com>
2026-08-17 16:35:44 +0100

    The test.

--- lambda_project/lambda_proj/tests/integration/test_api_gateway.py
+++ lambda_project/lambda_proj/tests/integration/test_api_gateway.py
@@ -7,7 +7,7 @@ import requests
 """
 Make sure env variable AWS_SAM_STACK_NAME exists with the name of the stack we are going to test. 
 """
-
+AWS_SAM_STACK_NAME = 'lambda_proj'
 
 class TestApiGateway:
 
